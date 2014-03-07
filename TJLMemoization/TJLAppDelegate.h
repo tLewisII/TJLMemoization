e235195a9970f04ef86413908816dd06df0231c2
Terry Lewis <terrylewis@outlook.com>
2014-03-06 20:01:46 -0600

    Initial Commit

---
+++ TJLMemoization/TJLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJLAppDelegate.h
+//  TJLMemoization
+//
+//  Created by Terry Lewis II on 3/6/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TJLMemoization/TJLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJLViewController.h
+//  TJLMemoization
+//
+//  Created by Terry Lewis II on 3/6/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLViewController : UIViewController
+
+@end
